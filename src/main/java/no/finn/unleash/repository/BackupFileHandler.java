4009ae529b845f4aa67eafafb84ec093ab1443df
Gard Rimestad <gardleopard@gmail.com>
2014-11-05 13:52:52 +0100

    Refactoring - move BackupFileHandler tests to BackupFileHandlerTest

--- src/main/java/no/finn/unleash/repository/BackupFileHandler.java
+++ src/main/java/no/finn/unleash/repository/BackupFileHandler.java
@@ -8,28 +8,35 @@ import java.util.Collections;
 
 class BackupFileHandler {
     private static final Logger LOG = LogManager.getLogger();
-    private static final String BACKUP_FILE =
-            System.getProperty("java.io.tmpdir") + File.separatorChar + "unleash-repo.json";
+    private final String backupFile;
+
+    BackupFileHandler(){
+        this(System.getProperty("java.io.tmpdir") + File.separatorChar + "unleash-repo.json");
+    }
+
+    BackupFileHandler(String backupFile){
+        this.backupFile = backupFile;
+    }
 
 
     ToggleCollection read() {
         LOG.info("Unleash will try to load feature toggle states from temporary backup");
-        try (FileReader reader = new FileReader(BACKUP_FILE)) {
+        try (FileReader reader = new FileReader(backupFile)) {
             BufferedReader br = new BufferedReader(reader);
             return JsonToggleParser.fromJson(br);
         } catch (FileNotFoundException e) {
-            LOG.warn("Unable to locate backup file:'{}'", BACKUP_FILE, e);
+            LOG.warn("Unable to locate backup file:'{}'", backupFile, e);
         } catch (IOException e) {
-            LOG.error("Failed to read backup file:'{}'", BACKUP_FILE, e);
+            LOG.error("Failed to read backup file:'{}'", backupFile, e);
         }
         return new ToggleCollection(Collections.EMPTY_LIST);
     }
 
     void write(ToggleCollection toggleCollection) {
-        try (FileWriter writer = new FileWriter(BACKUP_FILE)) {
+        try (FileWriter writer = new FileWriter(backupFile)) {
             writer.write(JsonToggleParser.toJsonString(toggleCollection));
         } catch (IOException e) {
-            LOG.warn("Unleash was unable to backup feature toggles to file: {}", BACKUP_FILE, e);
+            LOG.warn("Unleash was unable to backup feature toggles to file: {}", backupFile, e);
         }
     }
 }
---
+++ src/test/java/no/finn/unleash/repository/BackupFileHandlerTest.java
@@ -0,0 +1,44 @@
+package no.finn.unleash.repository;
+
+import no.finn.unleash.Toggle;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+
+public class BackupFileHandlerTest {
+
+    @Test
+    public void testRead() {
+        BackupFileHandler backupFileHandler = new BackupFileHandler(getClass().getResource("/unleash-repo.json").getFile());
+        ToggleCollection toggleCollection = backupFileHandler.read();
+        assertNotNull("presentFeature should be present", toggleCollection.getToggle("presentFeature"));
+    }
+
+    @Test
+    public void testReadWithoutFile() throws URISyntaxException {
+
+        BackupFileHandler backupFileHandler = new BackupFileHandler("/does/not/exist.json");
+        ToggleCollection toggleCollection = backupFileHandler.read();
+
+        assertNull("presentFeature should not be present", toggleCollection.getToggle("presentFeature"));
+    }
+
+    @Test
+    public void testWrite(){
+        ToggleCollection toggleCollection = new ToggleCollection(JsonToggleParser.fromJson("{\"features\": [{\"name\": \"writableFeature\",\"enabled\": true,\"strategy\": \"default\"}]}"));
+        String backupFile = System.getProperty("java.io.tmpdir") + File.separatorChar + "unleash-repo-test-write.json";
+        BackupFileHandler backupFileHandler = new BackupFileHandler(backupFile);
+        backupFileHandler.write(toggleCollection);
+        backupFileHandler = new BackupFileHandler(backupFile);
+        toggleCollection = backupFileHandler.read();
+        assertNotNull("writableFeature should be present", toggleCollection.getToggle("writableFeature"));
+
+
+    }
+
+}
