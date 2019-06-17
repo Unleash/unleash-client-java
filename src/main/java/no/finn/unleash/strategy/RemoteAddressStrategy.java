c00503f6c2cc1788a70cb3c61a4aacbb2ac94a3c
Jochen Schalanda <jochen@schalanda.name>
2019-06-17 12:15:54 +0200

    Add support for IPv6 and CIDR notation in RemoteAddressStrategy
    The `IpAddressMatcher` class has been taken from Spring Security 5.1.5.RELEASE (also Apache 2 licensed).
    
    Source: https://github.com/spring-projects/spring-security/blob/5.1.5.RELEASE/web/src/main/java/org/springframework/security/web/util/matcher/IpAddressMatcher.java
    
    Refs Unleash/unleash-client-node#58
    Closes #47

--- src/main/java/no/finn/unleash/strategy/RemoteAddressStrategy.java
+++ src/main/java/no/finn/unleash/strategy/RemoteAddressStrategy.java
@@ -1,16 +1,18 @@
 package no.finn.unleash.strategy;
 
+import no.finn.unleash.UnleashContext;
+import no.finn.unleash.util.IpAddressMatcher;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
-
-import no.finn.unleash.UnleashContext;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public final class RemoteAddressStrategy implements Strategy {
-
-    protected static final String PARAM = "IPs";
+    static final String PARAM = "IPs";
     private static final String STRATEGY_NAME = "remoteAddress";
-
+    private static final Pattern SPLITTER = Pattern.compile(",");
 
     @Override
     public String getName() {
@@ -25,9 +27,23 @@ public final class RemoteAddressStrategy implements Strategy {
     @Override
     public boolean isEnabled(Map<String, String> parameters, UnleashContext context) {
         return Optional.ofNullable(parameters.get(PARAM))
-                .map(ips -> Arrays.asList(ips.split(",\\s*")))
-                .map(ips -> ips.contains(context.getRemoteAddress().orElse(null)))
+                .map(ips -> Arrays.asList(SPLITTER.split(ips, -1)))
+                .map(ips -> ips.stream()
+                        .flatMap(ipAddress -> buildIpAddressMatcher(ipAddress)
+                                .map(Stream::of)
+                                .orElseGet(Stream::empty))
+                        .map(subnet -> context.getRemoteAddress()
+                                .map(subnet::matches)
+                                .orElse(false))
+                        .anyMatch(Boolean.TRUE::equals))
                 .orElse(false);
     }
 
+    private Optional<IpAddressMatcher> buildIpAddressMatcher(String ipAddress) {
+        try {
+            return Optional.of(new IpAddressMatcher(ipAddress));
+        } catch (IllegalArgumentException ex) {
+            return Optional.empty();
+        }
+    }
 }
