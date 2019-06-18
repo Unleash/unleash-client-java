f9f523e6e83a1a031266e76989259a5093bc21a7
Jochen Schalanda <jochen+github@schalanda.name>
2019-06-18 20:52:49 +0200

    feat: Add support for IPv6 and CIDR notation in RemoteAddressStrategy (#79)
    The `IpAddressMatcher` class has been taken from Spring Security 5.1.5.RELEASE (also Apache 2 licensed).
    
    Source: https://github.com/spring-projects/spring-security/blob/5.1.5.RELEASE/web/src/main/java/org/springframework/security/web/util/matcher/IpAddressMatcher.java
    
    Refs Unleash/unleash-client-node#58
    Closes #47

--- src/test/java/no/finn/unleash/strategy/RemoteAddressStrategyTest.java
+++ src/test/java/no/finn/unleash/strategy/RemoteAddressStrategyTest.java
@@ -1,11 +1,6 @@
 package no.finn.unleash.strategy;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
-
+import com.google.common.collect.ImmutableList;
 import no.finn.unleash.UnleashContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,44 +8,87 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class RemoteAddressStrategyTest {
-    private static final String FIRST_IP = "127.0.0.1";
-    private static final String SECOND_IP = "10.0.0.1";
-    private static final String THIRD_IP = "196.0.0.1";
-    private static final List<String> ALL = Arrays.asList(FIRST_IP, SECOND_IP, THIRD_IP);
+class RemoteAddressStrategyTest {
+    private static final String FIRST_IPV4 = "127.0.0.1";
+    private static final String SECOND_IPV4 = "10.0.0.1";
+    private static final String THIRD_IPV4 = "196.0.0.1";
+    private static final String FOURTH_IPV4 = "192.168.42.23";
+    private static final String IPV4_SUBNET = "192.168.0.0/16";
+    private static final List<String> ALL_IPV4 = Arrays.asList(FIRST_IPV4, SECOND_IPV4, THIRD_IPV4, IPV4_SUBNET);
+
+    private static final String FIRST_IPV6 = "::1";
+    private static final String SECOND_IPV6 = "2001:DB8:0:0:0:0:0:1";
+    private static final String THIRD_IPV6 = "2001:DB8::1";
+    private static final String IPV6_SUBNET = "2001:DB8::/48";
+    private static final List<String> ALL_IPV6 = Arrays.asList(FIRST_IPV6, SECOND_IPV6, THIRD_IPV6, IPV6_SUBNET);
+
+    private static final List<String> ALL = ImmutableList.<String>builder()
+            .addAll(ALL_IPV4)
+            .addAll(ALL_IPV6)
+            .build();
 
     private RemoteAddressStrategy strategy;
 
-    public static Stream<Arguments> data() {
+    static Stream<Arguments> data() {
         return Stream.of(
-                Arguments.of(FIRST_IP, FIRST_IP, true),
-                Arguments.of(FIRST_IP, String.join(",", ALL), true),
-                Arguments.of(SECOND_IP, String.join(",", ALL), true),
-                Arguments.of(THIRD_IP, String.join(",", ALL), true),
-                Arguments.of(FIRST_IP, String.join(", ", ALL), true),
-                Arguments.of(SECOND_IP, String.join(", ", ALL), true),
-                Arguments.of(THIRD_IP, String.join(", ", ALL), true),
-                Arguments.of(SECOND_IP, String.join(",  ", ALL), true),
-                Arguments.of(SECOND_IP, String.join(".", ALL), false),
-                Arguments.of(FIRST_IP, SECOND_IP, false));
+                Arguments.of(FIRST_IPV4, FIRST_IPV4, true),
+                Arguments.of(FIRST_IPV4, String.join(",", ALL_IPV4), true),
+                Arguments.of(SECOND_IPV4, String.join(",", ALL_IPV4), true),
+                Arguments.of(THIRD_IPV4, String.join(",", ALL_IPV4), true),
+                Arguments.of(FIRST_IPV4, String.join(", ", ALL_IPV4), true),
+                Arguments.of(SECOND_IPV4, String.join(", ", ALL_IPV4), true),
+                Arguments.of(THIRD_IPV4, String.join(", ", ALL_IPV4), true),
+                Arguments.of(SECOND_IPV4, String.join(",  ", ALL_IPV4), true),
+                Arguments.of(SECOND_IPV4, String.join(".", ALL_IPV4), false),
+                Arguments.of(FIRST_IPV4, SECOND_IPV4, false),
+                Arguments.of(FOURTH_IPV4, String.join(",", ALL_IPV4), true),
+                Arguments.of(FIRST_IPV4, IPV4_SUBNET, false),
+                Arguments.of(FIRST_IPV4, null, false),
+                Arguments.of(FIRST_IPV4, "", false),
+                Arguments.of(null, String.join(",", ALL_IPV4), false),
+
+                Arguments.of(FIRST_IPV6, FIRST_IPV6, true),
+                Arguments.of(FIRST_IPV6, String.join(",", ALL_IPV6), true),
+                Arguments.of(SECOND_IPV6, String.join(",", ALL_IPV6), true),
+                Arguments.of(THIRD_IPV6, String.join(",", ALL_IPV6), true),
+                Arguments.of(FIRST_IPV6, String.join(", ", ALL_IPV6), true),
+                Arguments.of(SECOND_IPV6, String.join(", ", ALL_IPV6), true),
+                Arguments.of(THIRD_IPV6, String.join(", ", ALL_IPV6), true),
+                Arguments.of(SECOND_IPV6, String.join(",  ", ALL_IPV6), true),
+                Arguments.of(SECOND_IPV6, String.join(".", ALL_IPV6), false),
+                Arguments.of(FIRST_IPV6, SECOND_IPV6, false),
+                Arguments.of(FIRST_IPV6, IPV6_SUBNET, false),
+                Arguments.of(FIRST_IPV6, null, false),
+                Arguments.of(FIRST_IPV6, "", false),
+                Arguments.of(null, String.join(".", ALL_IPV6), false),
+
+                Arguments.of(FIRST_IPV4, String.join(".", ALL), false),
+                Arguments.of(FIRST_IPV6, String.join(".", ALL), false));
     }
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         strategy = new RemoteAddressStrategy();
     }
 
     @Test
-    public void should_have_a_name() {
+    void should_have_a_name() {
         assertThat(strategy.getName(), is("remoteAddress"));
     }
 
     @ParameterizedTest
     @MethodSource("data")
-    public void test_all_combinations(String actualIp, String parameterString, boolean expected) {
+    void test_all_combinations(String actualIp, String parameterString, boolean expected) {
         UnleashContext context = UnleashContext.builder().remoteAddress(actualIp).build();
         Map<String, String> parameters = setupParameterMap(parameterString);
 
@@ -58,6 +96,10 @@ public class RemoteAddressStrategyTest {
     }
 
     private Map<String, String> setupParameterMap(String ipString) {
+        if (ipString == null) {
+            return Collections.emptyMap();
+        }
+        
         Map<String, String> parameters = new HashMap<>();
         parameters.put(RemoteAddressStrategy.PARAM, ipString);
         return parameters;
