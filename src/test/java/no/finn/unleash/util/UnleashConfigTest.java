6667d0ca8254f0e52b177e9ac5b41ee0d6b03392
Dennis Menge <dennis.menge@porsche.de>
2019-11-06 10:33:06 +0100

    Add support for http proxies that require basic authentication

--- src/test/java/no/finn/unleash/util/UnleashConfigTest.java
+++ src/test/java/no/finn/unleash/util/UnleashConfigTest.java
@@ -1,156 +1,208 @@
 package no.finn.unleash.util;
 
+import static no.finn.unleash.util.UnleashConfig.UNLEASH_APP_NAME_HEADER;
+import static no.finn.unleash.util.UnleashConfig.UNLEASH_INSTANCE_ID_HEADER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.Authenticator;
+import java.net.Authenticator.RequestorType;
 import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
 import java.net.URI;
 import java.net.URL;
+import no.finn.unleash.util.UnleashConfig.ProxyAuthenticator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-import static no.finn.unleash.util.UnleashConfig.UNLEASH_APP_NAME_HEADER;
-import static no.finn.unleash.util.UnleashConfig.UNLEASH_INSTANCE_ID_HEADER;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class UnleashConfigTest {
 
-    @Test
-    public void should_require_unleasAPI_url() {
-        Executable ex = () -> UnleashConfig.builder().appName("test").build();
-        assertThrows(IllegalStateException.class, ex);
-    }
+  @Test
+  public void should_require_unleasAPI_url() {
+    Executable ex = () -> UnleashConfig.builder().appName("test").build();
+    assertThrows(IllegalStateException.class, ex);
+  }
 
-    @Test
-    public void should_require_app_name() {
-        Executable ex = () -> UnleashConfig.builder().unleashAPI("http://unleash.com").build();
-        assertThrows(IllegalStateException.class, ex);
-    }
+  @Test
+  public void should_require_app_name() {
+    Executable ex = () -> UnleashConfig.builder().unleashAPI("http://unleash.com").build();
+    assertThrows(IllegalStateException.class, ex);
+  }
 
-    @Test
-    public void should_require_valid_uri() {
-        Executable ex = () -> UnleashConfig.builder().unleashAPI("this is not a uri").build();
-        assertThrows(IllegalArgumentException.class, ex);
-    }
+  @Test
+  public void should_require_valid_uri() {
+    Executable ex = () -> UnleashConfig.builder().unleashAPI("this is not a uri").build();
+    assertThrows(IllegalArgumentException.class, ex);
+  }
 
-    @Test
-    public void should_build_config() {
-        UnleashConfig config = UnleashConfig.builder()
-                .appName("my-app")
-                .instanceId("my-instance-1")
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_build_config() {
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .instanceId("my-instance-1")
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThat(config.getAppName(), is("my-app"));
-        assertThat(config.getInstanceId(), is("my-instance-1"));
-        assertThat(config.getUnleashAPI(), is(URI.create("http://unleash.org")));
-    }
+    assertThat(config.getAppName(), is("my-app"));
+    assertThat(config.getInstanceId(), is("my-instance-1"));
+    assertThat(config.getUnleashAPI(), is(URI.create("http://unleash.org")));
+  }
 
-    @Test
-    public void should_generate_backupfile() {
-        UnleashConfig config = UnleashConfig.builder()
-                .appName("my-app")
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_generate_backupfile() {
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThat(config.getAppName(), is("my-app"));
-        assertThat(config.getBackupFile(), is(System.getProperty("java.io.tmpdir") + File.separatorChar + "unleash-my-app-repo.json"));
-    }
+    assertThat(config.getAppName(), is("my-app"));
+    assertThat(config.getBackupFile(), is(System.getProperty("java.io.tmpdir") + File.separatorChar + "unleash-my-app-repo.json"));
+  }
 
-    @Test
-    public void should_use_provided_backupfile() {
-        UnleashConfig config = UnleashConfig.builder()
-                .appName("my-app")
-                .backupFile("/test/unleash-backup.json")
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_use_provided_backupfile() {
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .backupFile("/test/unleash-backup.json")
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThat(config.getAppName(), is("my-app"));
-        assertThat(config.getBackupFile(), is("/test/unleash-backup.json"));
-    }
+    assertThat(config.getAppName(), is("my-app"));
+    assertThat(config.getBackupFile(), is("/test/unleash-backup.json"));
+  }
 
-    @Test
-    public void should_set_sdk_version() {
-        UnleashConfig config = UnleashConfig.builder()
-                .appName("my-app")
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_set_sdk_version() {
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThat(config.getSdkVersion(), is("unleash-client-java:development"));
-    }
+    assertThat(config.getSdkVersion(), is("unleash-client-java:development"));
+  }
 
-    @Test
-    public void should_set_environment_to_default() {
-        UnleashConfig config = UnleashConfig.builder()
-                .appName("my-app")
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_set_environment_to_default() {
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThat(config.getEnvironment(), is("default"));
-    }
+    assertThat(config.getEnvironment(), is("default"));
+  }
 
-    @Test
-    public void should_set_environment() {
-        String env = "prod";
-        UnleashConfig config = UnleashConfig.builder()
-                .appName("my-app")
-                .environment(env)
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_set_environment() {
+    String env = "prod";
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .environment(env)
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThat(config.getEnvironment(), is(env));
-    }
+    assertThat(config.getEnvironment(), is(env));
+  }
 
-    @Test
-    public void should_add_app_name_and_instance_id_and_user_agent_to_connection() throws IOException {
-        String appName = "my-app";
-        String instanceId = "my-instance-1";
-        String unleashAPI = "http://unleash.org";
+  @Test
+  public void should_add_app_name_and_instance_id_and_user_agent_to_connection() throws IOException {
+    String appName = "my-app";
+    String instanceId = "my-instance-1";
+    String unleashAPI = "http://unleash.org";
 
-        UnleashConfig unleashConfig = UnleashConfig.builder()
-                .appName(appName)
-                .instanceId(instanceId)
-                .unleashAPI(unleashAPI)
-                .build();
+    UnleashConfig unleashConfig = UnleashConfig.builder()
+        .appName(appName)
+        .instanceId(instanceId)
+        .unleashAPI(unleashAPI)
+        .build();
 
-        URL someUrl = new URL(unleashAPI + "/some/arbitrary/path");
-        HttpURLConnection connection = (HttpURLConnection) someUrl.openConnection();
+    URL someUrl = new URL(unleashAPI + "/some/arbitrary/path");
+    HttpURLConnection connection = (HttpURLConnection) someUrl.openConnection();
 
-        UnleashConfig.setRequestProperties(connection, unleashConfig);
-        assertThat(connection.getRequestProperty(UNLEASH_APP_NAME_HEADER), is(appName));
-        assertThat(connection.getRequestProperty(UNLEASH_INSTANCE_ID_HEADER), is(instanceId));
-        assertThat(connection.getRequestProperty("User-Agent"), is(appName));
-    }
+    UnleashConfig.setRequestProperties(connection, unleashConfig);
+    assertThat(connection.getRequestProperty(UNLEASH_APP_NAME_HEADER), is(appName));
+    assertThat(connection.getRequestProperty(UNLEASH_INSTANCE_ID_HEADER), is(instanceId));
+    assertThat(connection.getRequestProperty("User-Agent"), is(appName));
+  }
 
-    @Test
-    public void should_add_custom_headers_to_connection_if_present() throws IOException {
-        String unleashAPI = "http://unleash.org";
-        String headerName = "UNLEASH-CUSTOM-TEST-HEADER";
-        String headerValue = "Some value";
+  @Test
+  public void should_add_custom_headers_to_connection_if_present() throws IOException {
+    String unleashAPI = "http://unleash.org";
+    String headerName = "UNLEASH-CUSTOM-TEST-HEADER";
+    String headerValue = "Some value";
 
-        UnleashConfig unleashConfig = UnleashConfig.builder()
-                .appName("my-app")
-                .instanceId("my-instance-1")
-                .unleashAPI(unleashAPI)
-                .customHttpHeader(headerName, headerValue)
-                .build();
+    UnleashConfig unleashConfig = UnleashConfig.builder()
+        .appName("my-app")
+        .instanceId("my-instance-1")
+        .unleashAPI(unleashAPI)
+        .customHttpHeader(headerName, headerValue)
+        .build();
 
-        URL someUrl = new URL(unleashAPI + "/some/arbitrary/path");
-        HttpURLConnection connection = (HttpURLConnection) someUrl.openConnection();
+    URL someUrl = new URL(unleashAPI + "/some/arbitrary/path");
+    HttpURLConnection connection = (HttpURLConnection) someUrl.openConnection();
 
-        UnleashConfig.setRequestProperties(connection, unleashConfig);
-        assertThat(connection.getRequestProperty(headerName), is(headerValue));
-    }
+    UnleashConfig.setRequestProperties(connection, unleashConfig);
+    assertThat(connection.getRequestProperty(headerName), is(headerValue));
+  }
 
-    @Test
-    public void should_require_instanceId() {
-        Executable ex = () ->  UnleashConfig.builder()
-                .appName("my-app")
-                .instanceId(null)
-                .unleashAPI("http://unleash.org")
-                .build();
+  @Test
+  public void should_require_instanceId() {
+    Executable ex = () -> UnleashConfig.builder()
+        .appName("my-app")
+        .instanceId(null)
+        .unleashAPI("http://unleash.org")
+        .build();
 
-        assertThrows(IllegalStateException.class, ex);
-    }
+    assertThrows(IllegalStateException.class, ex);
+  }
+
+  @Test
+  public void should_enable_proxy_based_on_jvm_settings()
+      throws IllegalAccessException, NoSuchFieldException {
+
+    String proxyHost = "proxy-host";
+    String proxyPort = "8080";
+    String proxyUser = "my-proxy-user";
+    String proxyPassword = "my-secret";
+
+    System.setProperty("http.proxyHost", proxyHost);
+    System.setProperty("http.proxyPort", proxyPort);
+    System.setProperty("http.proxyUser", proxyUser);
+    System.setProperty("http.proxyPassword", proxyPassword);
+
+    UnleashConfig config = UnleashConfig.builder()
+        .appName("my-app")
+        .unleashAPI("http://unleash.org")
+        .enableProxyAuthenticationByJvmProperties()
+        .build();
+
+    Field authenticator = Authenticator.class.getDeclaredField("theAuthenticator");
+    authenticator.setAccessible(true);
+    ProxyAuthenticator proxyAuthenticator = (ProxyAuthenticator) authenticator.get(null);
+
+    Field requestingAuthType = proxyAuthenticator.getClass().getSuperclass().getDeclaredField("requestingAuthType");
+    requestingAuthType.setAccessible(true);
+    requestingAuthType.set(proxyAuthenticator, RequestorType.PROXY);
+
+    Field requestingProtocol = proxyAuthenticator.getClass().getSuperclass().getDeclaredField("requestingProtocol");
+    requestingProtocol.setAccessible(true);
+    requestingProtocol.set(proxyAuthenticator, "http");
+
+    Field requestingHost = proxyAuthenticator.getClass().getSuperclass().getDeclaredField("requestingHost");
+    requestingHost.setAccessible(true);
+    requestingHost.set(proxyAuthenticator, proxyHost);
+
+    Field requestingPort = proxyAuthenticator.getClass().getSuperclass().getDeclaredField("requestingPort");
+    requestingPort.setAccessible(true);
+    requestingPort.set(proxyAuthenticator, 8080);
+
+    PasswordAuthentication passwordAuthentication = proxyAuthenticator.getPasswordAuthentication();
+
+    assertThat(passwordAuthentication.getUserName(), is(proxyUser));
+    assertThat(new String(passwordAuthentication.getPassword()), is(proxyPassword));
+    assertThat(config.isProxyAuthenticationByJvmProperties(), is(true));
+  }
 }
