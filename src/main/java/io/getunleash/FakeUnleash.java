d9fd9b62da4690c83683e17973988beb0a139f35
andreas-unleash <andreas@getunleash.ai>
2023-07-25 13:44:28 +0300

    remove unused method

--- src/main/java/io/getunleash/FakeUnleash.java
+++ src/main/java/io/getunleash/FakeUnleash.java
@@ -3,8 +3,6 @@ package io.getunleash;
 import static java.util.Collections.emptyList;
 
 import io.getunleash.lang.Nullable;
-import io.getunleash.strategy.*;
-import io.getunleash.variant.VariantUtil;
 import java.util.*;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
@@ -15,80 +13,6 @@ public class FakeUnleash implements Unleash {
     private Map<String, Boolean> excludedFeatures = new HashMap<>();
     private Map<String, FeatureToggle> features = new HashMap<>();
     private Map<String, Variant> variants = new HashMap<>();
-
-    private static final List<Strategy> BUILTIN_STRATEGIES =
-            Arrays.asList(
-                    new DefaultStrategy(),
-                    new ApplicationHostnameStrategy(),
-                    new GradualRolloutRandomStrategy(),
-                    new GradualRolloutSessionIdStrategy(),
-                    new GradualRolloutUserIdStrategy(),
-                    new RemoteAddressStrategy(),
-                    new UserWithIdStrategy(),
-                    new FlexibleRolloutStrategy());
-
-    private FeatureEvaluationResult getFeatureEvaluationResult(
-            String toggleName,
-            UnleashContext context,
-            BiPredicate<String, UnleashContext> fallbackAction,
-            @Nullable Variant defaultVariant) {
-        Optional<ActivationStrategy> enabledStrategy = Optional.empty();
-        boolean enabled = isEnabled(toggleName, context, fallbackAction);
-        if (features.containsKey(toggleName)) {
-            enabledStrategy =
-                    features.get(toggleName).getStrategies().stream()
-                            .filter(
-                                    s -> {
-                                        Optional<Strategy> strategy =
-                                                BUILTIN_STRATEGIES.stream()
-                                                        .filter(
-                                                                strategy1 ->
-                                                                        strategy1
-                                                                                .getName()
-                                                                                .equals(
-                                                                                        s
-                                                                                                .getName()))
-                                                        .findFirst();
-
-                                        return strategy.map(
-                                                        value ->
-                                                                value.isEnabled(
-                                                                        s.getParameters(),
-                                                                        context,
-                                                                        s.getConstraints()))
-                                                .orElse(false);
-                                    })
-                            .findFirst();
-        }
-        FeatureEvaluationResult result = new FeatureEvaluationResult(enabled, null);
-        if (enabledStrategy.isPresent()) {
-            Optional<Strategy> strategy =
-                    BUILTIN_STRATEGIES.stream()
-                            .filter(strategy1 -> strategy1.getName().equals(strategy1.getName()))
-                            .findFirst();
-
-            Strategy configuredStrategy = strategy.get();
-            result =
-                    configuredStrategy.getResult(
-                            enabledStrategy.get().getParameters(),
-                            context,
-                            enabledStrategy.get().getConstraints(),
-                            enabledStrategy.get().getVariants());
-        }
-
-        Variant variant = result.isEnabled() ? result.getVariant() : null;
-        // If strategy variant is null, look for a variant in the featureToggle
-        if (variant == null && defaultVariant != null) {
-            variant =
-                    result.isEnabled()
-                            ? VariantUtil.selectVariant(
-                                    features.get(toggleName), context, defaultVariant)
-                            : defaultVariant;
-        }
-        result.setVariant(variant);
-
-        return result;
-    }
 
     @Override
     public boolean isEnabled(String toggleName, boolean defaultSetting) {
