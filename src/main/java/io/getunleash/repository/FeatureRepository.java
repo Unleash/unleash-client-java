e432f4147d0a2f3e5f0b750dbce25db457f39415
Gastón Fournier <gaston@getunleash.io>
2023-11-21 11:58:27 +0100

    chore: allow to fail if unavailable

--- src/main/java/io/getunleash/repository/FeatureRepository.java
+++ src/main/java/io/getunleash/repository/FeatureRepository.java
@@ -95,7 +95,9 @@ public class FeatureRepository implements IFeatureRepository {
         }
 
         if (unleashConfig.isSynchronousFetchOnInitialisation()) {
-            updateFeatures(null).run();
+            updateFeatures(e -> {
+                throw e;
+            }).run();
         }
 
         if (!unleashConfig.isDisablePolling()) {
@@ -112,7 +114,7 @@ public class FeatureRepository implements IFeatureRepository {
         return Integer.max(20, 300 / Integer.max(fetchTogglesInterval, 1));
     }
 
-    private Runnable updateFeatures(@Nullable final Consumer<UnleashException> handler) {
+    private Runnable updateFeatures(final Consumer<UnleashException> handler) {
         return () -> {
             if (throttler.performAction()) {
                 try {
@@ -129,7 +131,12 @@ public class FeatureRepository implements IFeatureRepository {
 
                         featureBackupHandler.write(featureCollection);
                     } else if (response.getStatus() == ClientFeaturesResponse.Status.UNAVAILABLE) {
-                        throttler.handleHttpErrorCodes(response.getHttpStatusCode());
+                        if (!ready && unleashConfig.isSynchronousFetchOnInitialisation()) {
+                            throw new UnleashException(String.format("Could not initialize Unleash, got response code %d", response.getHttpStatusCode()), null);
+                        }
+                        if (ready) {
+                            throttler.handleHttpErrorCodes(response.getHttpStatusCode());
+                        }
                         return;
                     }
                     throttler.decrementFailureCountAndResetSkips();
@@ -138,11 +145,7 @@ public class FeatureRepository implements IFeatureRepository {
                         ready = true;
                     }
                 } catch (UnleashException e) {
-                    if (handler != null) {
-                        handler.accept(e);
-                    } else {
-                        throw e;
-                    }
+                    handler.accept(e);
                 }
             } else {
                 throttler.skipped(); // We didn't do anything this iteration, just reduce the count
